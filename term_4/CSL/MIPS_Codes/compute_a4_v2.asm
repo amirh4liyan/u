b14fcff5264b348659571b62897f1968aeddeb2a
Amirhossein Alian <alianmail81@gmail.com>
2025-03-31 09:49:59 +0330

    new code which uses less register and have termination command at the end

--- term_4/CSL/MIPS_Codes/compute_a4_v2.asm
+++ term_4/CSL/MIPS_Codes/compute_a4_v2.asm
@@ -8,18 +8,20 @@ main:
 	lw $t0, 0($s0) #‌load A[0]
 	addi $s0, $s0, 4 # offset A[1]
 	lw $t1, 0($s0) #‌load A[1]
-	add $t2, $t1, $t0 #‌ $t2 = A[0] +‌ A[1]
+	add $t0, $t0, $t1 #‌ $t0 = A[0] +‌ A[1]
 
 	addi $s0, $s0, 4 # offset A[2]
-	lw $t3, 0($s0) #‌load A[2]
+	lw $t1, 0($s0) #‌load A[2]
 	addi $s0, $s0, 4 # offset A[3]
-	lw $t4, 0($s0) #‌load A[3]
-	add $t5, $t4, $t3 #‌ $t5 = A[2] +‌ A[3]
+	lw $t2, 0($s0) #‌load A[3]
+	add $t1, $t1, $t2 #‌ $t1 = A[2] +‌ A[3]
 	
-	sub $t2, $t2, $t5 
-	sw $t2, 4($s0)
+	sub $t0, $t0, $t1 
+	sw $t0, 4($s0)
 	
 	li $v0, 1
-	addi $a0, $s0, 4
-	lw $a0, 0($a0)
+	add $a0, $t0, $zero
+	syscall
+	
+	li $v0, 17
 	syscall
