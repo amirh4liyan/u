bcfa3cc10c50e848bb70feae2bc517fa58dd5cf6
Amirhossein Alian <alianmail81@gmail.com>
2025-04-05 14:50:33 +0330

    add time complexity

--- term_4/DS/prj1/weighted_disorder.cpp
+++ term_4/DS/prj1/weighted_disorder.cpp
@@ -9,25 +9,26 @@ vector<int> per;
 int main()
 {
 	int n;
-	cin >> n;
+	cin >> n; // O(1)
 
 	int x;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // O(n)
 	{
-		cin >> x;
-		per.push_back(x);
+		cin >> x; // O(1)
+		per.push_back(x); // O(1)
 	}
 
 	int ans = 0;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // O(n^2)
 	{
 		for (int j = i+1; j < n; j++)
 		{
-			if (per[i] > per[j])
-				ans += j - i;
-			counter++;
+			if (per[i] > per[j]) // O(1)
+			{
+				ans += j - i; // O(1)
+			}
 		}
 	}
-	cout << ans;
+	cout << ans; // O(1)
 	return 0;
 }
